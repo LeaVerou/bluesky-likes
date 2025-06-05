fcc0b581997c4dd7e4669aeba52cfba56cf204ce
Lea Verou <lea@verou.me>
2025-06-05 10:59:28 -0400

    Update styles.js

--- src/likes/styles.js
+++ src/likes/styles.js
@@ -3,10 +3,11 @@ export default `
 	display: inline-flex;
 	gap: 0.25em;
 	color: #1185fe;
-	border: max(1px, .08em) solid currentColor;
+	border: max(1px, .07em) solid currentColor;
 	border-radius: 0.25em;
-	padding: 0.2em 0.35em;
+	padding: 0.15em 0.3em;
 	text-decoration: none;
+	font-weight: bold;
 }
 
 @keyframes loading {
@@ -24,12 +25,7 @@ a {
 }
 
 [part=icon] {
-	inline-size: 1.2em;
-	vertical-align: -0.2em;
-}
-
-#count {
-	font-weight: bold;
-	color: var(--likes-color-text);
+	block-size: 1em;
+	vertical-align: -0.16em;
 }
 `;
