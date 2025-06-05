e01b1267284a6f09e0047710fe1b09158e215caf
Lea Verou <lea@verou.me>
2025-06-05 12:59:47 -0400

    More elaborate autoloader

--- src/autoload.js
+++ src/autoload.js
@@ -1,10 +1,50 @@
-const components = ["likes", "likers"];
+export const components = ["likes", "likers"];
+let observer = null;
 
 if (globalThis.document) {
-	for (let component of components) {
-		let tag = `bluesky-${component}`;
-		if (document.querySelector(tag)) {
-			import(`./${component}/index.js`);
+	discover(document.body);
+}
+
+export function observe (node) {
+	observer ??= new MutationObserver(mutations => {
+		for (const { addedNodes } of mutations) {
+			for (const node of addedNodes) {
+				if (node.nodeType === Node.ELEMENT_NODE) {
+					discover(node);
+				}
+			}
 		}
+	});
+	observer.observe(node, { childList: true, subtree: true });
+}
+
+export function unobserve (node) {
+	observer.unobserve(node);
+}
+
+export function discover (node) {
+	for (let i = 0; i < components.length; i++) {
+		let component = components[i];
+		let tag = `bluesky-${component}`;
+		let isRegistered = Boolean(customElements.get(tag));
+
+		if (isRegistered) {
+			// Only check components that are not already defined
+			components.splice(i, 1);
+			i--;
+			continue;
+		}
+
+		if (node.querySelector(tag) || node.matches?.(tag)) {
+			import(`./${component}/index.js`);
+			components.splice(i, 1);
+			i--;
+		}
+	}
+
+	if (components.length === 0 && observer) {
+		// Nothing left to do here
+		observer.disconnect();
+		observer = null;
 	}
 }
